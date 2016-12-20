1d1683f3efaa4bd0baeeef7af00127e798aab336
Joshua Maddox <joshua@cinderhousecreative.com>
2016-12-20 23:05:24 +0800

    added some script and mark-up slyly hoisted off of wordpress docs into index.php

--- wp-content/themes/weekendplaything/header-main.php
+++ wp-content/themes/weekendplaything/header-main.php
@@ -9,12 +9,12 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <!-- Gets the name of the site as is set in Settings > General -->
+  <!-- Gets the name of the site as is set in Settings > General via the Template Tag bloginfo() -->
   <title><?php bloginfo( 'name' ); ?></title> 
 </head>
 <body>
   <div class="weekend-main-wrapper">
-    <h1>Here is the main-header.php file being pulled from header.php</h1>
+    <h1>This Header in header-main.php</h1>
     <!-- Function to add the search bar -->
     <?php get_search_form(); ?>
   </div>
--- wp-content/themes/weekendplaything/index.php
+++ wp-content/themes/weekendplaything/index.php
@@ -13,6 +13,41 @@ get_header();
   </div>
 </div>
 
+<!-- The Wordpress loop structure that loops through each post in the data base and displays data based on
+the Template Tags we use -->
+
+<!-- // Start the Loop. -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+ 
+        <!-- // Display the post's title. -->
+        <h2><?php the_title() ?></h2>
+ 
+        <!-- // Display a link to other posts by this posts author. -->
+        <small><?php _e( 'Posted by ', 'textdomain' ); the_author_posts_link() ?></small>
+ 
+        <!-- // Display the post's content in a div. -->
+        <div class="entry">
+            <?php the_content() ?>
+        </div>
+ 
+        <!-- // Display a comma separated list of the post's categories. -->
+        <?php _e( 'Posted in ', 'textdomain' ); the_category( ', ' ); ?>
+ 
+    <!-- // closes the first div box with the class of "post" or "post-cat-three" -->
+    </div>
+ 
+<!-- // Stop the Loop, but allow for a "if not posts" situation -->
+<?php endwhile; else :
+/*
+ * The very first "if" tested to see if there were any posts to
+ * display. This "else" part tells what do if there weren't any.
+ */
+_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+ // Completely stop the Loop.
+ endif;
+?>
+
+
 <!-- User get_template_part to access some template file -->
 <?php get_template_part('tpl-cinderhouse-portfolio-piece'); ?>
 
